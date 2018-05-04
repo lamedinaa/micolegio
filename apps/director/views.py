ac85edaecb43a94eda847105e176edde152cfc42
alejandro <lamedinaa@gmail.com>
2018-05-03 19:41:55 -0500

    formulario de edicion 2

--- apps/director/views.py
+++ apps/director/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from apps.director.forms import *
 from django.db import transaction,IntegrityError
 from django.db.models import Q
+import os
 
 @login_required
 def editarAlumno(request,id_alumno):
@@ -123,27 +124,45 @@ def insertFormEstudiantes(request):
 
 @login_required
 def editarEstudianteForm(request):
-        if request.FILES:
-            u = request.FILES['imgEstudiante'].name.split(".")[1]
-            extension = u.encode('ascii','ignore')
-            lastAlumnoId = str(Alumnos.objects.latest("id").id + 1)
-            request.FILES["imgEstudiante"].name= lastAlumnoId +"."+ extension
-        alumnos_form = AlumnosForm(request.POST,request.FILES)
-        if alumnos_form.is_valid():
-            try:
-                with transaction.atomic():
-                    escuela = request.user.perfil.escuela.all()[0]
-                    listaPadres = request.POST.getlist("padresId")
-                    query = Q()
-                    for padre in listaPadres:
-                        query  = query|Q(id = int(padre))
-                    padresObj = User.objects.filter(query)
-                    alumno = alumnos_form.save()
-                    alumno.escuela.add(escuela)
-                    for padre in padresObj:
-                        alumno.padres.add(padre)
-                    alumno.save()
-            except:
-                pass
+        #
+        # alumno = Alumnos.objects.prefetch_related('padres').get(id = request.POST['id_alumno'])
+        # alumno.nombres = request.POST["nombres"]
+        # alumno.apellidos = request.POST["apellidos"]
+        # alumno.numDocumento = request.POST["numDocumento"]
+        # alumno.email = request.POST["email"]
+        # ### ME FALTA EDITAR FECHA DE NACIMIENTO!!!!!
+        #
+        # ##editando padres del estudiante
+        # idListaPadres = request.POST.getlist('padresId')
+        # query = Q()
+        # for idPadre in idListaPadres:
+        #     query = query|Q(id = int(idPadre))
+        # padresObj = User.objects.filter(query)
+        # alumno.padres.all().delete()
+        # for padre in padresObj:
+        #     alumno.padres.add(padre.id)
+        # ####editando la imagen si existe
+        # if request.FILES:
+        #     u = request.FILES['imgEstudiante'].name.split(".")[1]
+        #     extension = u.encode('ascii','ignore')
+        #     lastAlumnoId = str(Alumnos.objects.latest("id").id + 1)
+        #     request.FILES["imgEstudiante"].name= lastAlumnoId +"."+ extension
+        #     path_inicial = alumno.imgAlumno.path
+        #     path_nuevo  = 'imgAlumno/' + request.FILES["imgEstudiante"].name
+        #     os.rename(path_inicial,path_nuevo)
+        # alumno.save()
+        # # try:
+        # #     with transaction.atomic():
+        # #         print "procesando formulario"
+        # #         alumno = Alumnos.objects.prefetch_related('padres').get(id = request.POST['id_alumno'])
+        # #         idListaPadres = request.POST.getlist('padresId')
+        # #         for idPadre in idListaPadres:
+        # #             query = query|Q(id = int(idPadre))
+        # #         padresObj = User.objects.filter(query)
+        # #
+        # #
+        # # except:
+        # #     print "no registra"
+        # #     pass
         data = {"msj":"registro exitoso"}
         return JsonResponse(data)
