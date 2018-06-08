d123cb469c242f0489dd8eba06f6174d0af2cb8d
unknown <lamedinaa@gmail.com>
2018-06-08 13:09:12 -0500

    cambios front y login

--- micolegio/settings.py
+++ micolegio/settings.py
@@ -79,16 +79,31 @@ CACHE_BACKEND = 'simple://'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'miColegio',
+#        'USER': 'postgres',
+#        'PASSWORD': '80777727',
+#        'HOST': '127.0.0.1',
+#        'PORT': 5432,
+#    }
+#}
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'miColegio',
         'USER': 'alejandro',
-        'PASSWORD': '80777727',
+        'PASSWORD': 'Martin80777727',
         'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'PORT': '',
     }
 }
+
+
+
 
 # AUTH_USER_MODEL = 'director.User'
 
